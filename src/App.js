3e2d63d32b8d3989da7fe89dfa26a49b553a6cbb
Tejas Subramanya K R <tejassujathagnks@gmail.com>
2024-09-11 00:08:56 +0530

    Removed Auth

--- src/App.js
+++ src/App.js
@@ -1,28 +1,10 @@
-import React, { useState } from 'react';
-import { Navigate, Route, Routes } from 'react-router-dom';
-import RefrshHandler from './RefrshHandler';
-import Login from './pages/Login';
-import Signup from './pages/Signup';
-import Home from './pages/Home';
+import React from 'react';
 import GameCanvas from './components/GameCanvas';
 
 function App() {
-  const [isAuthenticated, setIsAuthenticated] = useState(false);
-
-  const PrivateRoute = ({ element }) => {
-    return isAuthenticated ? element : <Navigate to="/login" />
-  }
-
   return (
     <div className="App">
-      <RefrshHandler setIsAuthenticated={setIsAuthenticated} />
-      <Routes>
-        <Route path='/' element={<Navigate to="/login" />} />
-        <Route path='/login' element={<Login />} />
-        <Route path='/signup' element={<Signup />} />
-        <Route path='/home' element={<PrivateRoute element={<Home />} />} />
-        <Route path='/child' element={<GameCanvas />} />
-      </Routes>
+      <GameCanvas />
     </div>
   );
 }
--- src/RefrshHandler.js
+++ src/RefrshHandler.js
@@ -1,25 +1,19 @@
-import React, { useEffect } from 'react'
-import { useLocation, useNavigate } from 'react-router-dom'
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import RefrshHandler from './RefrshHandler';
+import Home from './pages/Home';  // Adjusted import for Home
+import Login from './pages/Login'; // Adjusted import for Login
+import Signup from './pages/Signup'; // Adjusted import for Signup
 
-function RefrshHandler({ setIsAuthenticated }) {
-    const location = useLocation();
-    const navigate = useNavigate();
-
-    useEffect(() => {
-        if (localStorage.getItem('token')) {
-            setIsAuthenticated(true);
-            if (location.pathname === '/' ||
-                location.pathname === '/login' ||
-                location.pathname === '/signup'
-            ) {
-                navigate('/home', { replace: false });
-            }
-        }
-    }, [location, navigate, setIsAuthenticated])
-
-    return (
-        null
-    )
+function App() {
+  return (
+    <Router>
+      <RefrshHandler setIsAuthenticated={() => {}} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+    </Router>
+  );
 }
 
-export default RefrshHandler
+export default App;
