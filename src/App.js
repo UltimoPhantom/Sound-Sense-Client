9b0c06be7f27c6badb57b4f2462cc84d190bef8c
Tejas Subramanya K R <tejassujathagnks@gmail.com>
2024-09-28 01:10:12 +0530

    UPDATE: After login -> redirect to /home (instead of /child)

--- src/App.js
+++ src/App.js
@@ -87,7 +87,7 @@ function App() {
           {/* Update default route to LandingPage */}
           <Route path='/' element={<LandingPage />} />
 
-          <Route path='/login' element={isAuthenticated ? <Navigate to="/child" /> : <Login setIsAuthenticated={setIsAuthenticated} />} />
+          <Route path='/login' element={isAuthenticated ? <Navigate to="/home" /> : <Login setIsAuthenticated={setIsAuthenticated} />} />
 
           <Route path='/signup' element={<Signup />} />
 
