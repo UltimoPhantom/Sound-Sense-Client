36c5203fd0279118f671f38d2b7a101ba5bd7b6c
Tejas Subramanya K R <tejassujathagnks@gmail.com>
2024-10-15 11:45:49 +0530

    ADD: Contents

--- src/components/treasureContent.js
+++ src/components/treasureContent.js
@@ -4,7 +4,7 @@ import image1 from '../images/l1_img1.jpg';
 import image2 from '../images/l1_img2.png';
 import image3 from '../images/l1_img3.jpg';
 
-const createTreasureContent = (platforms) => [
+const createTreasureContent = () => [
 //   {
 //     x: 960, // Assuming 128x128 for treasure image
 //     y: 290,
@@ -15,26 +15,26 @@ const createTreasureContent = (platforms) => [
 //     letter: 'ಆ'
 //     // audio: audio1,
 //   },
-  {
-    x: 2870,
-    y: 90,
-    imageClosed: box_close_png,
-    imageOpen: box_open_png,
-    taskDescription: "Pronounce the above word!",
-    image: image2,
-    letter: 'ಈ'
-    // audio: audio1,
-  },
-  {
-    x: 5970,
-    y: 290,
-    imageClosed: box_close_png,
-    imageOpen: box_open_png,
-    taskDescription: "Repeat the phrase 'I can speak!'",
-    image: image3,
-    letter: 'ಏ'
-    // audio: audio1,
-  },
+{
+  x: 2870,
+  y: 90,
+  imageClosed: box_close_png,
+  imageOpen: box_open_png,
+  taskDescription: "Pronounce the above word!",
+  letterImage: image1,
+  letter: "lete111"
+  // audio: audio1,
+},
+{
+  x: 5970,
+  y: 290,
+  imageClosed: box_close_png,
+  imageOpen: box_open_png,
+  taskDescription: "Repeat the phrase 'I can speak!'",
+  letterImage: image3,
+  letter: 'ಏ' 
+  // audio: audio1,
+},
 ];
 
 export default createTreasureContent;
