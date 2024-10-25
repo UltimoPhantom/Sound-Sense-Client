3147c3fab71d21269715f58544f5c8f7c210213c
Tejas Subramanya K R <tejassujathagnks@gmail.com>
2024-10-25 20:36:31 +0530

    ADD: New Treasure Boxes with locations

--- src/components/treasureContent.js
+++ src/components/treasureContent.js
@@ -5,36 +5,76 @@ import image2 from '../images/l1_img2.png';
 import image3 from '../images/l1_img3.jpg';
 
 const createTreasureContent = () => [
-//   {
-//     x: 960, // Assuming 128x128 for treasure image
-//     y: 290,
-//     imageClosed: box_close_png,
-//     imageOpen: box_open_png,
-//     taskDescription: "Say the word 'apple' correctly!",
-//     image: image1,
-//     letter: 'ಆ'
-//     // audio: audio1,
-//   },
-{
-  x: 2870,
-  y: 90,
-  imageClosed: box_close_png,
-  imageOpen: box_open_png,
-  taskDescription: "Pronounce the above word!",
-  letterImage: image1,
-  letter: "ಆ",
-  // audio: audio1,
-},
-{
-  x: 5970,
-  y: 290,
-  imageClosed: box_close_png,
-  imageOpen: box_open_png,
-  taskDescription: "Repeat the phrase 'I can speak!'",
-  letterImage: image3,
-  letter: 'ಹಾ' 
-  // audio: audio1,
-},
+  //   {
+  //     x: 960, // Assuming 128x128 for treasure image
+  //     y: 290,
+  //     imageClosed: box_close_png,
+  //     imageOpen: box_open_png,
+  //     taskDescription: "Say the word 'apple' correctly!",
+  //     image: image1,
+  //     letter: 'ಆ'
+  //     // audio: audio1,
+  //   },
+  { //1
+    x: 2870,
+    y: 90,
+    imageClosed: box_close_png,
+    imageOpen: box_open_png,
+    taskDescription: "Pronounce the above word!",
+    letterImage: image1,
+    letter: "ಆ",
+    // audio: audio1,
+  },
+  { //2
+    x: 5970,
+    y: 290,
+    imageClosed: box_close_png,
+    imageOpen: box_open_png,
+    taskDescription: "Repeat the phrase 'I can speak!'",
+    letterImage: image3,
+    letter: 'ಹಾ'
+    // audio: audio1,
+  },
+  { //3
+    x: 7870,
+    y: 139-30,
+    imageClosed: box_close_png,
+    imageOpen: box_open_png,
+    taskDescription: "Repeat the phrase 'I can speak!'",
+    letterImage: image3,
+    letter: 'ಹಾ'
+    // audio: audio1,
+  },
+  { //4
+    x: 10410,
+    y: 319 - 30,
+    imageClosed: box_close_png,
+    imageOpen: box_open_png,
+    taskDescription: "Repeat the phrase 'I can speak!'",
+    letterImage: image3,
+    letter: 'ಹಾ'
+    // audio: audio1,
+  },
+  { //5
+    x: 13772 - 200,
+    y: 319 - 30,
+    imageClosed: box_close_png,
+    imageOpen: box_open_png,
+    taskDescription: "Repeat the phrase 'I can speak!'",
+    letterImage: image3,
+    letter: 'ಹಾ'
+    // audio: audio1,
+  },
+  { //6
+    x: 18037,
+    y: 49 - 30,
+    imageClosed: box_close_png,
+    imageOpen: box_open_png,
+    taskDescription: "Repeat the phrase 'I can speak!'",
+    letterImage: image3,
+    letter: 'ಹಾ'
+    // audio: audio1,
+  },
 ];
 
 export default createTreasureContent;
